e8143fb324c10558c58283532cf486409e5e0177
KanimozhiMurugesan26 <kanimozhiofficial92@gmail.com>
2025-10-18 15:24:17 -0400

    to check merge conflict

--- apit/src/pojo/Orders.java
+++ apit/src/pojo/Orders.java
@@ -13,7 +13,7 @@ public class Orders {
 	public void setOrders(List<OrderDetail> orders) {
 		this.orders = orders;
 
-		system.out.println("added for new branch");
+		system.out.println("merge conflict");
 	}
 	
 	
